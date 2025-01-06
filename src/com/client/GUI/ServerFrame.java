cafb53f991ee4098ea69b59552165c19b8ce6ff6
mshinay511@gmail.com <mshinay511@gmail.com>
2025-01-06 23:23:39 +0800

    Final Version

---
+++ src/com/client/GUI/ServerFrame.java
@@ -0,0 +1,78 @@
+package com.client.GUI;
+
+import com.client.Client;
+import com.client.User;
+import com.message.Message;
+import com.message.MessageType;
+import com.server.Server;
+import com.server.ServerSocketManager;
+import com.server.ThreatOfServerToClient;
+import com.utility.Utility;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerFrame extends JFrame {
+    private JTextArea txtAllMessages;
+    private JScrollPane scrollPaneAllMessages;
+
+    public ServerFrame() {
+
+        this.setTitle("Server");
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        init();
+        this.setVisible(true);
+    }
+    private void init() {
+        txtAllMessages = new JTextArea(30, 20);
+        txtAllMessages.setEditable(false);
+        scrollPaneAllMessages = new JScrollPane(txtAllMessages,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        this.add(scrollPaneAllMessages);
+        this.pack();
+
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerFrame serverFrame = new ServerFrame();
+
+        ServerSocket serverSocket = new ServerSocket(9999);//lisent to port 9999
+        while (true) {
+            ServerSocketManager.viewAllSocket();
+            Socket socket = serverSocket.accept();
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());//send Message to Client
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());//receive Message from Client
+            User user=(User) ois.readObject();
+            Message message = new Message();
+            if(!Utility.CheckUser(user.getId(),user.getPassword())) {
+                message.setMessageType(MessageType.MESSAGE_LOGIN_FAIL);
+                oos.writeObject(message);
+                socket.close();
+            }
+            if(ServerSocketManager.getSocket(user.getId())!=null) {//avoid Repeated logins
+                message.setMessageType(MessageType.MESSAGE_REPEATED_LOGIN);
+                oos.writeObject(message);
+                socket.close();
+            }
+            else {
+                message.setMessageType(MessageType.MESSAGE_LOGIN_SUCESS);//LOGIN_SUCESS
+                oos.writeObject(message);
+                ServerSocketManager.addSocket(user.getId(), socket);//add into Server's socket set
+                ThreatOfServerToClientGUI threatOfServerToClientGUI = new ThreatOfServerToClientGUI(socket, user);
+                threatOfServerToClientGUI.setTxtAllMessages(serverFrame.txtAllMessages);
+                new Thread(threatOfServerToClientGUI).start();
+                serverFrame.txtAllMessages.append(user.getId()+"进入服务器\n");
+
+            }
+
+        }
+
+    }
+
+}
---
+++ src/com/client/GUI/ThreatOfServerToClientGUI.java
@@ -0,0 +1,105 @@
+package com.client.GUI;
+
+
+import com.client.User;
+import com.message.Message;
+import com.message.MessageType;
+import com.server.ServerSocketManager;
+import com.utility.Utility;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Map;
+
+public class ThreatOfServerToClientGUI implements Runnable{
+    Socket socket;
+    User user;
+    private JTextArea txtAllMessages;
+
+    public void setTxtAllMessages(JTextArea txtAllMessages) {
+        this.txtAllMessages = txtAllMessages;
+    }
+
+    public ThreatOfServerToClientGUI(Socket socket, User user) {
+        this.socket = socket;
+        this.user=user;
+    }
+
+    @Override
+    public void run() {
+        ObjectInputStream ois;
+        boolean loop = true;
+
+
+        while (loop){
+
+
+            try {
+                    ois = new ObjectInputStream(socket.getInputStream());
+                    Message message=(Message) ois.readObject();
+                    String content="";
+                switch (message.getMessageType()){//deal with different types of message
+                    case MessageType.MESSAGE_SEND_GRUPE:
+                        content=(String) message.getContent();
+
+                        txtAllMessages.append(user.getId()+":"+content+"\t"+ Utility.TimeFormat(message.getSendtime())+"\n");
+                        Map.Entry[] entries = ServerSocketManager.getAllIfo();
+                        for (Map.Entry entry : entries) {
+                            ObjectOutputStream Receiveroos = new ObjectOutputStream(((Socket)entry.getValue()).getOutputStream());
+                            message.setReceiverID((String) entry.getKey());
+                            Receiveroos.writeObject(message);
+                            Receiveroos.flush();
+                        }
+                        break;
+                    case MessageType.MESSAGE_SEND_PRIVATE:
+                        if(ServerSocketManager.getSocket(message.getReceiverID())==null){
+                            message.setContent("当前用户不在线");
+                            message.setMessageType(MessageType.MESSAGE_SEND_PRIVATE_FAIL);
+                            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                            oos.writeObject(message);
+                            oos.flush();
+                        }
+                        else {
+                            Socket Receiversocket = ServerSocketManager.getSocket(message.getReceiverID());
+                            ObjectOutputStream Receiveroos = new ObjectOutputStream(Receiversocket.getOutputStream());
+                            Receiveroos.writeObject(message);
+                            Receiveroos.flush();
+                            txtAllMessages.append(message.getSenderID()+"对"+message.getReceiverID()+":"+message.getContent()+"\t"+ Utility.TimeFormat(message.getSendtime())+"\n");
+                        }
+                        break;
+                    case MessageType.MESSAGE_LOGOUT:
+                        loop = false;
+                        ServerSocketManager.removeSocket(user.getId());
+                        txtAllMessages.append(user.getId()+"下线\n");
+                        break;
+                    case MessageType.MESSAGE_VIEW_ONLINEUSER:
+                        message.setContent(ServerSocketManager.viewAllSocket());
+                        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                        System.out.println(ServerSocketManager.viewAllSocket());
+                        oos.writeObject(message);
+                        oos.flush();
+                        txtAllMessages.append(message.getSenderID()+"查询了在线用户\n");
+                        break;
+                }
+
+            } catch (Exception e) {
+                try {
+                    ServerSocketManager.removeSocket(user.getId());
+                } catch (IOException ex) {
+
+                }
+
+                txtAllMessages.append(user.getId()+"下线\n");
+                    break;
+                }
+
+            }
+
+
+        }
+    }
+
+
